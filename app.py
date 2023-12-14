e08e06fa5f8fb050a370b933df37ef70a512a408
fantinelli <fernandaapbercelino@gmail.com>
2023-12-14 10:39:07 -0300

    up

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+agendamentos = []
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', agendamentos=agendamentos)
+
+@app.route('/agendar', methods=['GET', 'POST'])
+def agendar():
+    if request.method == 'POSTs':
+        data = request.form['data']
+        horario = request.form['horario']
+        novo_agendamento = {'data': data, 'horario': horario}
+        agendamentos.append(novo_agendamento)
+        return jsonify({'message': 'Horário agendado com sucesso!'})
+
+    return render_template('agendar.html')
+
+@app.route('/agendamentos/<int:agendamento_id>', methods=['DELETE', 'PUT'])
+def gerenciar_agendamento(agendamento_id):
+    if request.method == 'DELETE':
+        if 0 <= agendamento_id < len(agendamentos):
+            del agendamentos[agendamento_id]
+            return jsonify({'message': 'Agendamento excluído com sucesso!'})
+        else:
+            return jsonify({'error': 'ID de agendamento inválido'}), 404
+
+    elif request.method == 'PUT':
+        if 0 <= agendamento_id < len(agendamentos):
+            data = request.form['data']
+            horario = request.form['horario']
+            agendamentos[agendamento_id]['data'] = data
+            agendamentos[agendamento_id]['horario'] = horario
+            return jsonify({'message': 'Agendamento atualizado com sucesso!'})
+        else:
+            return jsonify({'error': 'ID de agendamento inválido'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ script.js
@@ -0,0 +1,55 @@
+document.getElementById('agendamentoForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    const data = document.getElementById('data').value;
+    const horario = document.getElementById('horario').value;
+  
+    const agendamentoId = document.getElementById('agendamentoId').value;
+    const method = agendamentoId ? 'PUT' : 'POST';
+    const url = agendamentoId ? `/agendamentos/${agendamentoId}` : '/agendar';
+  
+    fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            data: data,
+            horario: horario
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+  
+        const recadoDiv = document.createElement('div');
+        recadoDiv.innerText = 'Horário agendado com sucesso!';
+        recadoDiv.className = 'recado';
+        document.body.appendChild(recadoDiv);
+  
+        document.getElementById('agendamentoForm').reset();
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+  });
+  
+  function excluirAgendamento(agendamentoId) {
+    fetch(`/agendamentos/${agendamentoId}`, {
+        method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+  }
+  
+  function editarAgendamento(agendamentoId, data, horario) {
+    document.getElementById('agendamentoId').value = agendamentoId;
+    document.getElementById('data').value = data;
+    document.getElementById('horario').value = horario;
+    document.getElementById('submitBtn').innerText = 'Atualizar';
+  }
